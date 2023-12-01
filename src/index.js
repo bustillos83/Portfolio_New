e5f02a9f0aa73ee73e0f705150233c451715710f
Fernando Bustillos <105611825+bustillos83@users.noreply.github.com>
2023-12-01 11:41:53 -0500

    react 17

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
+import ReactDom from "react-dom";
 
 
 import App from "./App";
 import "./index.css"; 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />)
+
+ReactDom.render(<App />, document.getElementById("root"))
