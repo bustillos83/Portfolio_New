80eab1eff0907b66581526de44d8b35e211eaf9c
Fernando Bustillos <105611825+bustillos83@users.noreply.github.com>
2023-12-07 16:00:34 -0500

    added headers

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState ,useEffect} from 'react';
 import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
@@ -8,44 +8,35 @@ import { urlFor, client } from '../../client';
 const About = () => {
   const [abouts, setAbouts] = useState([]);
 
-  useEffect(() => {
+  const fetchAbouts = async () => {
     const query = '*[_type == "abouts"]';
-
-    client.fetch(query).then((data) => {
-      setAbouts(data);
-    });
+    setAbouts(await client.fetch(query));
+  }
+  
+  useEffect(() => {
+    fetchAbouts();
   }, []);
   
   return (
     <>
-    <h2 className='head-text'>I Know that <span>Good Development</span><br/> Means <span>Good Business</span>
-
-    </h2>
-    <div className='app__profiles'>
-    {abouts.map((about, index)=>(
-      <motion.div
-      whileInView={{opacity:1}}
-      whileHover={{scale: 1.1}}
-      transition={{duration:0.5, type:"tween"}}
-      className='app__profile-item'
-      key={about.title + index}
-      >
-      <img src={urlFor(about.imgUrl)} alt={about.title}/>
-      <h2 className='bold-text' style={{marginTop:20}}>{about.title}</h2>
-      <p className='p-text' style={{marginTop:10}}>{about.description}
-      </p>
-      
-      </motion.div>
-    ))}
-
-    </div>
-
+      <h2 className='head-text'>I know that <span>Good Development</span><br />means <span>Good Business</span></h2>
+      <div className='app__profiles'>
+        {abouts.map((about,index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration:0.5, type:'tween' }}
+            className='app__profile-item'
+            key={about.title+index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
+            <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default AppWrap(
-  MotionWrap(About, "app__about"),
-  "about",
-  "app__whitebg"
-)
+export default AppWrap( MotionWrap(About, 'app__about'), 'about', 'app__whitebg');
