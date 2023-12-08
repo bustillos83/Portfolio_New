5768acaf8a51fb5590863de083959656e0f7cb68
Fernando Bustillos <105611825+bustillos83@users.noreply.github.com>
2023-12-08 09:57:10 -0500

    changed work section

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -43,7 +43,7 @@ const Work = () => {
   }
   return (
    <>
-    <h2 className='head-text'>My Creative <span>Portfolio </span> section </h2>
+    <h2 className='head-text'>See my past <span>work </span> </h2>
     <div className='app__work-filter'>
     {['UI/UX', 'Web App','Mobile App', 'React JS', 'All'].map((item, index) => (
       <div
