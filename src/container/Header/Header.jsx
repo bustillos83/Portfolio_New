6be0125b842483231fce2b85219e835131d1062b
Fernando Bustillos <105611825+bustillos83@users.noreply.github.com>
2023-12-22 09:46:44 -0500

    Change icons

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -65,7 +65,7 @@ const Header = () => {
     className="app__header-circles"
 
     >
-    {[images.flutter, images.redux, images.sass].map((circle, index)=>(
+    {[images.javascript, images.html, images.css, images.node].map((circle, index)=>(
       <div className='circle-cmp app__flex' key={`circle-${index}`}>
       <img src={circle} alt="circle"/>
       </div>
